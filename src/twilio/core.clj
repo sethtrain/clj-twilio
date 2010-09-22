9b3300e17648b2a192577cd7cdafcc150c90a314
Seth Buntin <seth.buntin@gmail.com>
2010-09-22 13:03:59 -0500

    twiml response xml generator via c.c.prxml

--- src/twilio/core.clj
+++ src/twilio/core.clj
@@ -1,1 +1,8 @@
-(ns twilio.core)
+(ns twilio.core
+  (:use [clojure.contrib.prxml :only [prxml]]))
+
+(defmacro twiml-response [& verbs]
+  `(with-out-str
+     (prxml
+      [:decl! {:version "1.1"}]
+      [:Response ~@verbs])))
--- test/twilio/test/core.clj
+++ test/twilio/test/core.clj
@@ -1,6 +1,18 @@
 (ns twilio.test.core
   (:use [twilio.core] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test]
+        [twilio.voice :only [say play gather]]))
 
-(deftest replace-me
-  (is false "No tests have been written."))
+(deftest test-twilio-voice-response
+  (is (= (twiml-response (say "Message"))
+         "<?xml version=\"1.1\" encoding=\"UTF-8\"?><Response><Say>Message</Say></Response>"))
+  (is (= (twiml-response (say "Message")
+                          (play "message.mp3"))
+         "<?xml version=\"1.1\" encoding=\"UTF-8\"?><Response><Say>Message</Say><Play>message.mp3</Play></Response>"))
+  (is (= (twiml-response (gather {:action "/process_gather.php" :method "GET"}
+                                  (say "Please enter your account number, followed by the pound sign")))
+         "<?xml version=\"1.1\" encoding=\"UTF-8\"?><Response><Gather action=\"/process_gather.php\" method=\"GET\"><Say>Please enter your account number, followed by the pound sign</Say></Gather></Response>"))
+  (is (= (twiml-response (gather {:action "/process_gather.php" :method "GET"}
+                                  (say "Please enter your account number, followed by the pound sign"))
+                          (say "We didn't receive any input. Goodbye!"))
+         "<?xml version=\"1.1\" encoding=\"UTF-8\"?><Response><Gather action=\"/process_gather.php\" method=\"GET\"><Say>Please enter your account number, followed by the pound sign</Say></Gather><Say>We didn&apos;t receive any input. Goodbye!</Say></Response>")))
