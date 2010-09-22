e4c640c3f34b3fb61d324a09016f5033f9a00e09
Seth Buntin <seth.buntin@gmail.com>
2010-09-22 13:15:32 -0500

    add :aot option for compilation; update jar name for easily pushing to clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.1"]])
+                     [swank-clojure "1.2.1"]]
+  :aot [twilio.core
+        twilio.voice]
+  :jar-name "clj-twilio.jar")
