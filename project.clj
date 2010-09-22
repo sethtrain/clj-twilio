62c1202f7816477f10dbbf0b2ed15e300feb39d9
Seth Buntin <seth.buntin@gmail.com>
2010-09-22 13:07:38 -0500

    add lein-clojars as dev-dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Clojure Twilio API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[lein-clojars "0.5.0"]
+                     [swank-clojure "1.2.1"]])
