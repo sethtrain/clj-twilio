8259c86600581bbcdb783e45a107393ddcee8f9a
Seth Buntin <seth.buntin@gmail.com>
2010-09-24 11:24:19 -0500

    bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-twilio "0.1.0"
+(defproject clj-twilio "0.2.0"
   :description "Clojure Twilio API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
