5e794386f524f9f6779c794b22a69ac49c633983
Dustin Getz <dgetz@wingspan.com>
2012-03-28 23:51:08 -0400

    fixed lein deps

--- project.clj
+++ project.clj
@@ -2,8 +2,6 @@
   :description "Clojure Twilio API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.1"]]
   :aot [twilio.core
         twilio.twiml.core
         twilio.twiml.sms
